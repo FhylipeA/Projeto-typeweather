5a9dc4d51cf67ecb54026ed035051693ba68755c
Fhylipe Adan <fhylipejobs@gmail.com>
2025-04-22 21:34:41 -0300

    Finalização

---
+++ src/services/getCityByNameService.ts
@@ -0,0 +1,39 @@
+import { log } from "node:console";
+import { api } from "./api";
+
+export interface CityProps {
+  id: string,
+  name: string,
+  latitude: number,
+  longitude: number,
+}
+
+export interface CityAPIResponse {
+  id: string;
+  name: string;
+  sys: {
+    country?: string;
+  };
+  coord: {
+    lat: number;
+    lon: number;
+  };
+}
+
+export async function getCityByNameService(name: string): Promise<CityProps[]> {
+  try {
+    const { data } = await api.get(`/weather?q=${name}`);
+    console.log(data.sys);
+
+    const city = {
+      id: data.id,
+      name: data.sys.country ? `${data.name}, ${data.sys.country}` : data.name,
+      longitude: data.coord.lon,
+      latitude: data.coord.lat,
+    };
+
+    return [city];
+  } catch (error) {
+    return [];
+  }
+}
